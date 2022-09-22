1e01ffc40e227415fc485763d5b0b3f29a6d5293
Claudio Assis <96958858+Cl84Dev@users.noreply.github.com>
2022-09-22 14:04:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
+let result = document.getElementById('resultado')
+let botaoDeCalcular = document.getElementById('calcular')
+botaoDeCalcular.addEventListener('click', calcular)
+
 function calcular() {
-    let aporteInicial = Number(document.getElementById('aporteInicial').value)
-    let aporteMensal = Number(document.getElementById('aporteMensal').value)
-    let periodo = Number(document.getElementById('periodo').value)
-    let taxaAnual = Number(document.getElementById('taxaAnual').value)
-    let taxaMensal = ((((taxaAnual / 100) + 1) ** (1 / 12)) - 1) * 100
-    let botaoDeCalcular = document.getElementById('calcular')
-    let reset = document.createElement('button')
-    let result = document.getElementById('res')
-    let ano = ""
-    reset.id = 'reset'
+  let aporteInicial = Number(document.getElementById('aporteInicial').value)
+  let aporteMensal = Number(document.getElementById('aporteMensal').value)
+  let anos = Number(document.getElementById('periodo').value)
+  let taxaAnual = Number(document.getElementById('taxaAnual').value)
+  let meses = anos * 12
+  let taxaMensal = Math.pow(1 + taxaAnual/100, 1/12) -1
+  let ano = anos === 1 ? 'ano' : 'anos' 
+  let rendimentoTotalSobreAporteInicial = Math.pow(1 + taxaAnual/100, anos) * aporteInicial
+  let recebidoEmJurosSobreAporteInicial = rendimentoTotalSobreAporteInicial - aporteInicial
+  let rendimentoTotalSobreAportesMensais = (((Math.pow(1 + taxaMensal, meses)) - 1) / (taxaMensal)) * aporteMensal
+  let totalDeAportesMensais = aporteMensal * meses
+  let recebidoEmJurosSobreAportesMensais = rendimentoTotalSobreAportesMensais - totalDeAportesMensais
+  let somaAporteInicialEMensais = totalDeAportesMensais + aporteInicial
+  let rendimentoTotal = rendimentoTotalSobreAporteInicial + rendimentoTotalSobreAportesMensais
+  let recebidoEmJurosSobreAportesInicialEMensais = rendimentoTotal - somaAporteInicialEMensais
+    
+  if ((aporteInicial === 0 && aporteMensal === 0) || aporteInicial < 0 || aporteMensal < 0 || anos <= 0 || taxaAnual <= 0) {
+    window.alert(`Preencha os campos com valores válidos. Você pode optar em informar apenas o aporte inicial ou o aporte mensal, mas não pode deixar ambos vazios.`)
 
-    reset.onclick = function() {
-      botaoDeCalcular.disabled = false
-      document.getElementById('aporteInicial').value = ''
-      document.getElementById('aporteMensal').value = ''
-      document.getElementById('periodo').value = ''
-      document.getElementById('taxaAnual').value = ''
-      document.getElementById('res').innerHTML = ''
-      document.getElementById('aporteInicial').focus()
-    }
+  } else {
+  
+    if ( aporteInicial !==0 && aporteMensal === 0) {
+      result.innerHTML =
+      `<h2>Resultado</h2>
+      <p>Se você fizer um aporte único de ${aporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} à uma taxa anual de ${taxaAnual}%, ao fim de ${anos} ${ano} você vai ter acumulado o total de ${rendimentoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>
+      <p>Valor investido: ${aporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+      <p>Valor recebido em juros: ${recebidoEmJurosSobreAporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+      <button id="reset" onclick=reiniciar() >Reiniciar Calculadora</button>`
+      botaoDeCalcular.disabled = true
 
-    if(periodo === 1) {
-      ano = "ano"
+    } else if ( aporteInicial === 0 && aporteMensal !== 0 ) {
+      result.innerHTML =
+      `<h2>Resultado</h2>
+      <p>Se você fizer aportes mensais de ${aporteMensal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} à uma taxa anual de ${taxaAnual}%, ao fim de ${anos} ${ano} você vai ter acumulado o total de ${rendimentoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>
+      <p>Valor investido: ${totalDeAportesMensais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+      <p>Valor recebido em juros: ${recebidoEmJurosSobreAportesMensais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+      <button id="reset" onclick=reiniciar() >Reiniciar Calculadora</button>`
+      botaoDeCalcular.disabled = true
+
     } else {
-      ano = "anos" 
-    }
-    
-    let rendimentoTotalSobreAporteInicial = (((taxaAnual / 100) + 1) ** periodo) * aporteInicial
-    let rendimentoTotalSobreAportesMensais = ((((1 +(taxaMensal / 100)) ** (periodo * 12)) - 1) / (taxaMensal / 100)) * aporteMensal
-    let recebidoEmJurosSobreAporteInicial = rendimentoTotalSobreAporteInicial - aporteInicial
-    let totalDeAportesMensais = aporteMensal * (periodo * 12)
-    let recebidoEmJurosSobreAportesMensais = rendimentoTotalSobreAportesMensais - totalDeAportesMensais
-    let aporteTotal = totalDeAportesMensais + aporteInicial
-    let rendimentoTotal = rendimentoTotalSobreAporteInicial + rendimentoTotalSobreAportesMensais
-    let totalInvestido = rendimentoTotal - aporteTotal
-    
-    if ((aporteInicial === 0 && aporteMensal === 0) || aporteInicial < 0 || aporteMensal < 0 || periodo <= 0 || taxaAnual <= 0) {
-       window.alert(`Preencha os campos com valores válidos. Você pode optar em informar apenas o aporte inicial ou o aporte mensal, mas não pode deixar ambos vazios.`)
-    } else {
-    
-      if ( aporteInicial !=0 && aporteMensal === 0) {
-        result.innerHTML = `<h2>Resultado</h2><p>Se você fizer um aporte único de ${aporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} à uma taxa anual de ${taxaAnual}%, ao fim de ${periodo} ${ano} você vai ter acumulado o total de ${rendimentoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>
-        <p>Valor investido: ${aporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p><p>Valor recebido em juros: ${recebidoEmJurosSobreAporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p> `
-        reset.innerHTML = "Reiniciar Calculadora"
-        result.appendChild(reset)
-        botaoDeCalcular.disabled = true
-      } else if ( aporteInicial == 0 && aporteMensal != 0 ) {
-        result.innerHTML = `<h2>Resultado</h2><p>Se você fizer aportes mensais de ${aporteMensal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} à uma taxa anual de ${taxaAnual}%, ao fim de ${periodo} ${ano} você vai ter acumulado o total de ${rendimentoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>
-        <p>Valor investido: ${totalDeAportesMensais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p><p>Valor recebido em juros: ${recebidoEmJurosSobreAportesMensais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>`
-        reset.innerHTML = "Reiniciar Calculadora"
-        result.appendChild(reset)
-        botaoDeCalcular.disabled = true
-      } else {
-        result.innerHTML = `<h2>Resultado</h2><p>Se você fizer um aporte inicial de ${aporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} e aportes mensais de ${aporteMensal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} a uma taxa anual de ${taxaAnual}%, ao fim de ${periodo} ${ano} você vai ter acumulado o total de ${rendimentoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>
-        <p>Valor investido: ${aporteTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p><p>Valor recebido em juros: ${totalInvestido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>`
-        reset.innerHTML = "Reiniciar Calculadora"
-        result.appendChild(reset)
-        botaoDeCalcular.disabled = true
-      }
+      result.innerHTML =
+      `<h2>Resultado</h2>
+      <p>Se você fizer um aporte inicial de ${aporteInicial.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} e aportes mensais de ${aporteMensal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} a uma taxa anual de ${taxaAnual}%, ao fim de ${anos} ${ano} você vai ter acumulado o total de ${rendimentoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>
+      <p>Valor investido: ${somaAporteInicialEMensais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p><p>Valor recebido em juros: ${recebidoEmJurosSobreAportesInicialEMensais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+      <button id="reset" onclick=reiniciar() >Reiniciar Calculadora</button>`
+      botaoDeCalcular.disabled = true
 
     }
-    
+
+  }
     
 }
 
-document.getElementById('calcular').addEventListener('click', calcular)
+function reiniciar() {
+  botaoDeCalcular.disabled = false
+  result.innerHTML = ''
+  document.getElementById('aporteInicial').value = ''
+  document.getElementById('aporteMensal').value = ''
+  document.getElementById('periodo').value = ''
+  document.getElementById('taxaAnual').value = ''
+  document.getElementById('aporteInicial').focus()
+}
